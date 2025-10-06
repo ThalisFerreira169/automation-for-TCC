d4a907a4c4e970c10333c57a50ceac1cba95f0b5
Thalis Ferreira <thalisferreira169@gmail.com>
2025-10-06 09:21:58 -0300

    Aplicação de automação com GUI para TCC - Ajuste nomeclatura de pastas

---
+++ simular-escrita-3/typeText.c
@@ -0,0 +1,56 @@
+#include <windows.h>
+
+void pressKey(WORD vk)
+{
+    keybd_event(vk, 0, 0, 0);
+    keybd_event(vk, 0, KEYEVENTF_KEYUP, 0);
+}
+
+int isCapsLockOn()
+{
+    return (GetKeyState(VK_CAPITAL) & 0x0001);
+}
+
+void type(const char* text)
+{
+    int capsLockOn = isCapsLockOn();
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        char c = text[i];
+
+        SHORT vkNormal = VkKeyScan(c);
+        INPUT input[2] = {0};
+
+        int isUpper = (vkNormal & 0x0100) != 0;
+
+        if (isUpper && !capsLockOn) {
+            keybd_event(VK_SHIFT, 0, 0, 0);
+        }
+
+        input[0].type = INPUT_KEYBOARD;
+        input[0].ki.wVk = (WORD)vkNormal;
+        input[0].ki.dwFlags = 0;
+
+        input[1].type = INPUT_KEYBOARD;
+        input[1].ki.wVk = (WORD)vkNormal;
+        input[1].ki.dwFlags = KEYEVENTF_KEYUP;
+
+        SendInput(2, input, sizeof(INPUT));
+
+        if (isUpper && !capsLockOn) {
+            keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0);
+        }
+
+        Sleep(100);
+    }
+}
+
+int main ()
+{
+    Sleep(3000);
+
+    const char* text = "Hello, this is a test for my typing tool. I hope you enjoy this tool!";
+    type(text);
+
+    return 0;
+}
